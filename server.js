c7b5c420d29e9ed2344d025e90de2d879e39c33c
jfranklin12 <j.franks725@gmail.com>
2022-08-07 13:53:12 -0400

    initial route created and connection set in server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const model = require('./models');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 const PORT = process.env.PORT || 3001;
 
