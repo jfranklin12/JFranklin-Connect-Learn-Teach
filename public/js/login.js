36e6c24422fcac311f29449cf296a4bd7bf7028e
jfranklin12 <j.franks725@gmail.com>
2022-08-08 09:21:57 -0400

    signup function works between front end and back end

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,19 @@
-const signUpHandler = (event) => {
+const signUpHandler = async (event) => {
     event.preventDefault();
     const username = document.getElementById('username').value.trim();
 
-    const password = document.getElementById('password').value.trim();
-
     const email = document.getElementById('email').value.trim();
 
+    const password = document.getElementById('password').value.trim();
 
-
-    console.log(username);
-    console.log(password);
-    console.log(email);
+    if(username && email && password) {
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response);
+    };
 };
 
 document.getElementById('signup-submit').addEventListener('click', signUpHandler);
