f6ef507706af4dcffe78848e57e00342b4d6b9f8
jfranklin12 <j.franks725@gmail.com>
2022-08-09 18:11:37 -0400

    comments routse working and delete post route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const postRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
 
 // /api/user
 router.use('/user', userRoutes);
 
 // /api/post
 router.use('/post', postRoutes);
+
+// /api/post
+router.use('/comment', commentRoutes);
 
 module.exports = router;
