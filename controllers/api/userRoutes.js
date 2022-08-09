8ae624b6a982392387b77b00aeafb304a0e58ef5
jfranklin12 <j.franks725@gmail.com>
2022-08-09 10:01:04 -0400

    login function added and sign up adjusted

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = dbUser.id;
             req.session.logged_in = true;
-            res.status(200).json({ message: 'You are now loggen in!'});
+            res.status(200).json({ message: 'You are now logged in!'});
         });
     } catch (err) {
         res.status(500).json(err);
--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,34 @@
+// Function to login
+const loginHandler = async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('login-username').value.trim();
+
+    const email = document.getElementById('login-email').value.trim();
+
+    const password = document.getElementById('login-password').value.trim();
+
+    if(username && email && password) {
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response);
+    }
+};
+
+document.getElementById('login-submit').addEventListener('click', loginHandler);
+
+
+
+// Function to sign up
 const signUpHandler = async (event) => {
     event.preventDefault();
-    const username = document.getElementById('username').value.trim();
+    const username = document.getElementById('signup-username').value.trim();
 
-    const email = document.getElementById('email').value.trim();
+    const email = document.getElementById('signup-email').value.trim();
 
-    const password = document.getElementById('password').value.trim();
+    const password = document.getElementById('signup-password').value.trim();
 
     if(username && email && password) {
         const response = await fetch('/api/user', {
