a09b86b23a16bbb4637e711f7b8e750cffb354b9
jfranklin12 <j.franks725@gmail.com>
2022-08-10 09:54:36 -0400

    homepage shows date created

--- public/js/logout.js
+++ public/js/logout.js
@@ -18,3 +18,24 @@ const logoutHandler = async () => {
     }
 };
 document.getElementById('logout').addEventListener('click', logoutHandler);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems);
+    console.log(instances);
+});
+
+const logoutHandlerNav = async () => {
+    console.log('button click');
+    const response = await fetch('/api/user/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+document.getElementById('logout_navbar').addEventListener('click', logoutHandlerNav);
